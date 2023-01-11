e571744be347fd30ffe2157c003da10e891dce8d
EricOrtegaGi <114953110+EricOrtegaGi@users.noreply.github.com>
2023-01-11 20:32:29 +0100

    Update select2.sql

--- select2.sql
+++ select2.sql
@@ -1,3 +1,41 @@
-SELECT locations.city, locations.location_id, departments.location_id
-FROM locations JOIN departments
-ON locations.location_id = departments.location_id
+/*1*/
+
+SELECT L.city, COUNT(D.department_id)
+FROM locations L
+JOIN departments D ON L.location_id = D.location_id
+GROUP BY L.city
+
+/*2*/
+
+SELECT L.city, COUNT(D.department_id)
+FROM locations L
+LEFT JOIN departments D ON L.location_id = D.location_id
+GROUP BY L.city;
+
+/*3*/
+
+SELECT L.city, COUNT(D.department_id) 
+FROM locations L
+JOIN departments D ON L.location_id = D.location_id
+GROUP BY L.city
+HAVING COUNT(D.department_id) > 1
+
+
+/*4*/
+
+SELECT d.department_name, c.country_id , COUNT(e.employee_id), AVG(e.salary)
+FROM departments d
+JOIN employees e ON d.department_id = e.department_id
+JOIN locations l ON d.location_id = l.location_id
+JOIN countries c ON l.country_id = c.country_id
+GROUP BY d.department_name, c.country_id
+ORDER BY c.country_id ASC, AVG(e.salary) DESC;
+
+
+/*5*/
+/*6*/
+/*7*/
+/*8*/
+/*9*/
+/*10*/
+
